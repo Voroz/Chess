5b104160785578def914fed7585ef9426b7bbb9f
Voroz <jonny_fherm@hotmail.com>
2016-08-07 13:56:30 +0200

    Implemented Queen and Bishop movement.

--- Chess/Bishop.cpp
+++ Chess/Bishop.cpp
@@ -14,5 +14,69 @@ Bishop::~Bishop() {
 }
 std::vector<Tile*> Bishop::possibleMoves() {
 	std::vector<Tile*> tempVec;
+	std::array<std::array<Tile*, 8>, 8> tiles = _currTile->board()->tiles();
+	Vector2<int> index = _currTile->index();
+
+	//Diagonal top left
+	Vector2<int> testIndex = Vector2<int>(index.x - 1, index.y - 1);
+	while (testIndex.x >= 0 && testIndex.y >= 0 &&
+		tiles[testIndex.x][testIndex.y]->holding() == nullptr) {
+
+		tempVec.push_back(tiles[testIndex.x][testIndex.y]);
+		testIndex.x--;
+		testIndex.y--;
+	}
+	if (testIndex.x >= 0 && testIndex.y >= 0 &&
+		tiles[testIndex.x][testIndex.y]->holding() != nullptr &&
+		&tiles[testIndex.x][testIndex.y]->holding()->owner() != &_owner) {
+		tempVec.push_back(tiles[testIndex.x][testIndex.y]);
+	}
+
+	//Diagonal top right
+	testIndex = Vector2<int>(index.x + 1, index.y - 1);
+	while (testIndex.x < 8 && testIndex.y >= 0 &&
+		tiles[testIndex.x][testIndex.y]->holding() == nullptr) {
+
+		tempVec.push_back(tiles[testIndex.x][testIndex.y]);
+		testIndex.x++;
+		testIndex.y--;
+	}
+	if (testIndex.x < 8 && testIndex.y >= 0 &&
+		tiles[testIndex.x][testIndex.y]->holding() != nullptr &&
+		&tiles[testIndex.x][testIndex.y]->holding()->owner() != &_owner) {
+		tempVec.push_back(tiles[testIndex.x][testIndex.y]);
+	}
+
+	//Diagonal bottom left
+	testIndex = Vector2<int>(index.x - 1, index.y + 1);
+	while (testIndex.x >= 0 && testIndex.y < 8 &&
+		tiles[testIndex.x][testIndex.y]->holding() == nullptr) {
+
+		tempVec.push_back(tiles[testIndex.x][testIndex.y]);
+		testIndex.x--;
+		testIndex.y++;
+	}
+	if (testIndex.x >= 0 && testIndex.y < 8 &&
+		tiles[testIndex.x][testIndex.y]->holding() != nullptr &&
+		&tiles[testIndex.x][testIndex.y]->holding()->owner() != &_owner) {
+		tempVec.push_back(tiles[testIndex.x][testIndex.y]);
+	}
+
+	//Diagonal bottom right
+	testIndex = Vector2<int>(index.x + 1, index.y + 1);
+	while (testIndex.x < 8 && testIndex.y < 8 &&
+		tiles[testIndex.x][testIndex.y]->holding() == nullptr) {
+
+		tempVec.push_back(tiles[testIndex.x][testIndex.y]);
+		testIndex.x++;
+		testIndex.y++;
+	}
+	if (testIndex.x < 8 && testIndex.y < 8 &&
+		tiles[testIndex.x][testIndex.y]->holding() != nullptr &&
+		&tiles[testIndex.x][testIndex.y]->holding()->owner() != &_owner) {
+		tempVec.push_back(tiles[testIndex.x][testIndex.y]);
+	}
+
+
 	return tempVec;
 }
--- Chess/Queen.cpp
+++ Chess/Queen.cpp
@@ -14,5 +14,124 @@ Queen::~Queen() {
 }
 std::vector<Tile*> Queen::possibleMoves() {
 	std::vector<Tile*> tempVec;
+	std::array<std::array<Tile*, 8>, 8> tiles = _currTile->board()->tiles();
+	Vector2<int> index = _currTile->index();
+
+	// Diagonal top left
+	Vector2<int> testIndex = Vector2<int>(index.x - 1, index.y - 1);
+	while (testIndex.x >= 0 && testIndex.y >= 0 &&
+		tiles[testIndex.x][testIndex.y]->holding() == nullptr) {
+
+		tempVec.push_back(tiles[testIndex.x][testIndex.y]);
+		testIndex.x--;
+		testIndex.y--;
+	}
+	if (testIndex.x >= 0 && testIndex.y >= 0 &&
+		tiles[testIndex.x][testIndex.y]->holding() != nullptr &&
+		&tiles[testIndex.x][testIndex.y]->holding()->owner() != &_owner) {
+		tempVec.push_back(tiles[testIndex.x][testIndex.y]);
+	}
+
+	// Diagonal top right
+	testIndex = Vector2<int>(index.x + 1, index.y - 1);
+	while (testIndex.x < 8 && testIndex.y >= 0 &&
+		tiles[testIndex.x][testIndex.y]->holding() == nullptr) {
+
+		tempVec.push_back(tiles[testIndex.x][testIndex.y]);
+		testIndex.x++;
+		testIndex.y--;
+	}
+	if (testIndex.x < 8 && testIndex.y >= 0 &&
+		tiles[testIndex.x][testIndex.y]->holding() != nullptr &&
+		&tiles[testIndex.x][testIndex.y]->holding()->owner() != &_owner) {
+		tempVec.push_back(tiles[testIndex.x][testIndex.y]);
+	}
+
+	// Diagonal bottom left
+	testIndex = Vector2<int>(index.x - 1, index.y + 1);
+	while (testIndex.x >= 0 && testIndex.y < 8 &&
+		tiles[testIndex.x][testIndex.y]->holding() == nullptr) {
+
+		tempVec.push_back(tiles[testIndex.x][testIndex.y]);
+		testIndex.x--;
+		testIndex.y++;
+	}
+	if (testIndex.x >= 0 && testIndex.y < 8 &&
+		tiles[testIndex.x][testIndex.y]->holding() != nullptr &&
+		&tiles[testIndex.x][testIndex.y]->holding()->owner() != &_owner) {
+		tempVec.push_back(tiles[testIndex.x][testIndex.y]);
+	}
+
+	// Diagonal bottom right
+	testIndex = Vector2<int>(index.x + 1, index.y + 1);
+	while (testIndex.x < 8 && testIndex.y < 8 &&
+		tiles[testIndex.x][testIndex.y]->holding() == nullptr) {
+
+		tempVec.push_back(tiles[testIndex.x][testIndex.y]);
+		testIndex.x++;
+		testIndex.y++;
+	}
+	if (testIndex.x < 8 && testIndex.y < 8 &&
+		tiles[testIndex.x][testIndex.y]->holding() != nullptr &&
+		&tiles[testIndex.x][testIndex.y]->holding()->owner() != &_owner) {
+		tempVec.push_back(tiles[testIndex.x][testIndex.y]);
+	}
+
+	// Left
+	testIndex = Vector2<int>(index.x - 1, index.y);
+	while (testIndex.x >= 0 &&
+		tiles[testIndex.x][testIndex.y]->holding() == nullptr) {
+
+		tempVec.push_back(tiles[testIndex.x][testIndex.y]);
+		testIndex.x--;
+	}
+	if (testIndex.x >= 0 &&
+		tiles[testIndex.x][testIndex.y]->holding() != nullptr &&
+		&tiles[testIndex.x][testIndex.y]->holding()->owner() != &_owner) {
+		tempVec.push_back(tiles[testIndex.x][testIndex.y]);
+	}
+
+	// Right
+	testIndex = Vector2<int>(index.x + 1, index.y);
+	while (testIndex.x < 8 &&
+		tiles[testIndex.x][testIndex.y]->holding() == nullptr) {
+
+		tempVec.push_back(tiles[testIndex.x][testIndex.y]);
+		testIndex.x++;
+	}
+	if (testIndex.x < 8 &&
+		tiles[testIndex.x][testIndex.y]->holding() != nullptr &&
+		&tiles[testIndex.x][testIndex.y]->holding()->owner() != &_owner) {
+		tempVec.push_back(tiles[testIndex.x][testIndex.y]);
+	}
+
+	// Top
+	testIndex = Vector2<int>(index.x, index.y - 1);
+	while (testIndex.y >= 0 &&
+		tiles[testIndex.x][testIndex.y]->holding() == nullptr) {
+
+		tempVec.push_back(tiles[testIndex.x][testIndex.y]);
+		testIndex.y--;
+	}
+	if (testIndex.y >= 0 &&
+		tiles[testIndex.x][testIndex.y]->holding() != nullptr &&
+		&tiles[testIndex.x][testIndex.y]->holding()->owner() != &_owner) {
+		tempVec.push_back(tiles[testIndex.x][testIndex.y]);
+	}
+
+	// Bottom
+	testIndex = Vector2<int>(index.x, index.y + 1);
+	while (testIndex.y < 8 &&
+		tiles[testIndex.x][testIndex.y]->holding() == nullptr) {
+
+		tempVec.push_back(tiles[testIndex.x][testIndex.y]);
+		testIndex.y++;
+	}
+	if (testIndex.y < 8 &&
+		tiles[testIndex.x][testIndex.y]->holding() != nullptr &&
+		&tiles[testIndex.x][testIndex.y]->holding()->owner() != &_owner) {
+		tempVec.push_back(tiles[testIndex.x][testIndex.y]);
+	}
+
 	return tempVec;
 }
